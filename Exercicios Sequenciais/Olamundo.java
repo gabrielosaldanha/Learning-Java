e3e8a494b9cf920992da854bb9686ad8ad56739b
Gabriel Saldanha <gabrielsaldanha1990@gmail.com>
2017-02-28 11:00:39 -0300

    Create Olamundo.java

---
+++ Exercicios Sequenciais/Olamundo.java
@@ -0,0 +1,7 @@
+//Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+public class Olamundo {
+    public static void main(String[] args) {
+        
+        System.out.println("Olá Mundo");
+    }
+}
